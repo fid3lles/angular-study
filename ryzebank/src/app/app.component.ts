04fa6169554e57cffb043c1ec23a7495d9751788
Gabriel Fidelles <gabrielfidellesc@hotmail.com>
2021-10-18 01:02:23 -0300

    New component "app-extrato" and style changes

--- ryzebank/src/app/app.component.ts
+++ ryzebank/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ryzebank';
+  transferencias: any[] = [];
+
+  transferir($event: any) {
+    console.log($event);
+    const transferencia = { ...$event, data: new Date() };
+    this.transferencias.push(transferencia);
+  }
 }
--- ryzebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ ryzebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent {}
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitado nova transferencia.');
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
